a78873886a65cbb0c3c27298b89ee82f54e8d483
Manasses Goncalves <manasses.goncalves@gmail.com>
2021-02-11 23:28:05 -0300

    feat: start text robot

---
+++ robots/text.js
@@ -0,0 +1,59 @@
+const algorithmia = require('algorithmia') //importado modulo do algorithmia
+const algorithmiaApiKey = require('../credencials/algorithmia.json').apiKey
+const sentenceBoundaryDetectation = require('sbd')
+
+async function robot(content) {
+    await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    breakContentIntoSentences(content)
+  
+    async function fetchContentFromWikipedia(content){      
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey) //retorna instancia autenticada (troca-se pelo API gerado no site algorithmia)
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm) //recebe como parametro a busca
+        const wikipediaContent = wikipediaResponde.get() //recebe o conteudo da pesquisa... no caso o da Wikipedia
+
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+       
+        content.sourceContentSanitized = withoutDatesInParentheses
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+            
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                    return false
+                }
+
+                return true
+            })
+
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+    }
+
+    function removeDatesInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/    /g,'')
+    }
+
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+        const sentences = sentenceBoundaryDetectation.sentences(content.sourceContentSanitized)
+        sentences.forEach((sentence) => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
+    }
+
+}
+
+module.exports = robot
